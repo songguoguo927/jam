969c8714108e3eada6bd0b4bdaae7486bd8a7370
songguoguo927 <1351443467@qq.com>
2019-07-04 15:49:17 +0800

    实现篇文章数据渲染，设计父子组件数据传递

--- src/components/ArticleList/article.js
+++ src/components/ArticleList/article.js
@@ -1,32 +1,35 @@
-import React,{Component} from 'react';
+import React, { Component } from "react";
 
 export default class Article extends Component {
-    constructor(props){
-        super()
-    }
-        // console.log(props.articleList);
-        // props.articleList.map(article=>console.log(article))
-        //article是我们要的数据=》一篇文章的相关信息
-       
-       render(){
-        return (
-            <div className="article">
-          <h4>{this.props.articleList[0].title}</h4>
-          <span><i className="iconfont icon-75"></i>{this.props.articleList[0].date}</span>
-          <ul>
-            <li className="fix-icon">
-              <i className="iconfont icon-biaoqian"></i>
-            </li>
-            {/* 渲染标签 */}
-            {
-                this.props.articleList[0].tags.map(tag=><li>{tag}</li>)
-            }
-          </ul>
-          <p>{this.props.articleList[0].shortcut}</p>
-          <p>
-          {this.props.articleList[0].articleContent}
-          </p>
-        </div>
-        )
-       }
-    }
+  constructor(props) {
+    super();
+  }
+
+  // props.articleList.map(article=>console.log(article))
+  //article是我们要的数据=》一篇文章的相关信息
+
+  render() {
+    return (
+      // <div>{this.props.articleItem.title}</div>
+      // console.log(this.props.articleItem)
+      <div className="article">
+        <h4>{this.props.articleItem.title}</h4>
+        <span>
+          <i className="iconfont icon-75" />
+          {this.props.articleItem.date}
+        </span>
+        <ul>
+          <li className="fix-icon">
+            <i className="iconfont icon-biaoqian" />
+          </li>
+          {/* 渲染标签 */}
+          {this.props.articleItem.tags.map(tag => (
+            <li>{tag}</li>
+          ))}
+        </ul>
+        <p>{this.props.articleItem.shortcut}</p>
+        <p>{this.props.articleItem.articleContent}</p>
+      </div>
+    );
+  }
+}
