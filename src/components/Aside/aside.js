c6cb5e88177d7cdffdc6bdece73ca8bc65fd7d95
songguoguo927 <1351443467@qq.com>
2019-07-04 12:59:54 +0800

    完成侧边导航栏数据渲染

---
+++ src/components/Aside/aside.js
@@ -0,0 +1,98 @@
+import React, { Component } from "react";
+import Recommand from "./recommand";
+export default class Aside extends Component {
+  render(props) {
+    const datas = [
+      {
+        type: "category",
+        title: "分类",
+        contents: [
+          {
+            name: "web前端",
+            nums: "52"
+          },
+          {
+            name: "node",
+            nums: "4"
+          },
+          {
+            name: "移动端",
+            nums: "42"
+          },
+          {
+            name: "服务端",
+            nums: "12"
+          },
+          {
+            name: "计算机网络",
+            nums: "92"
+          },
+          {
+            name: "算法",
+            nums: "62"
+          }
+        ]
+      },
+      {
+        type: "time",
+        title: "归档",
+        contents: [
+          {
+            name: "2019",
+            nums: "34"
+          },
+          {
+            name: "2018",
+            nums: "33"
+          },
+          {
+            name: "2017",
+            nums: "84"
+          }
+        ]
+      },
+      {
+        type: "link",
+        title: "链接",
+        contents: [
+          { name: "react官方文档",nums:null },
+          { name: "MDN" ,nums:null},
+          { name: "youtube",nums:null }
+        ]
+      }
+    ];
+    const asideParts = datas.map(data => {
+      //   console.log(data);
+      return (
+        <div className={`aside-part ${data.type}`} key={data.type}>
+          <h3>{data.title}</h3>
+
+          <ul>
+            {data.contents.map(content => {
+                // console.log(content)
+                // const haveNums = `${content.nums}`
+                return(            
+              <li key={content.name}>
+                <a href="jjj">{content.name}</a>
+              <span className="nums"> {`${content.nums}`==null ?  '' :`(${content.nums})` }</span> 
+              </li>
+                )
+            }
+            )}
+          </ul>
+        </div>
+      );
+    });
+    return (
+      <div className="aside">
+        <div className="follow-me">
+          关注我：
+          <i className="iconfont icon-github" />
+          <a href="https://github.com/songguoguo927/">Github</a>
+        </div>
+        {asideParts}        
+        <Recommand />
+      </div>
+    );
+  }
+}
